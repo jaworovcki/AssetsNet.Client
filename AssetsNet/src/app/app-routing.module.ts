9008e4426f5a25f613a6711f37cd36ba62e7cce8
Denys Leon <leonden0046@gmail.com>
2024-03-12 00:28:33 +0200

    Fixed routing

--- AssetsNet/src/app/app-routing.module.ts
+++ AssetsNet/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: "home", component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
