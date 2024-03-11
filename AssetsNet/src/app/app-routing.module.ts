d8833d69d4843189b50b98cb11d3fd05dfab3a4b
Andrii <jaworovcki@gmail.com>
2024-03-11 18:18:02 +0200

    add routings to home

--- AssetsNet/src/app/app-routing.module.ts
+++ AssetsNet/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/account/login/login.component';
 import { RegisterComponent } from './components/account/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: "home", component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
