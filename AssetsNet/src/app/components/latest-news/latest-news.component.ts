b7d811e986df5b353ee81a9a2f95ff2a00873ea4
Denys Leon <leonden0046@gmail.com>
2024-03-14 19:30:27 +0200

    Added latest-news component

---
+++ AssetsNet/src/app/components/latest-news/latest-news.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-latest-news',
+  templateUrl: './latest-news.component.html',
+  styleUrls: ['./latest-news.component.scss']
+})
+export class LatestNewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
