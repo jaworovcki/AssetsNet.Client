3023625db9fcfb98be1173160fb70d57e82b12b2
Denys Leon <leonden0046@gmail.com>
2024-03-14 20:39:16 +0200

    Added the ability to choose company

--- AssetsNet/src/app/components/latest-news/latest-news.component.ts
+++ AssetsNet/src/app/components/latest-news/latest-news.component.ts
@@ -8,17 +8,35 @@ import { News } from 'src/app/models/news/news';
   styleUrls: ['./latest-news.component.scss']
 })
 export class LatestNewsComponent implements OnInit {
-
+  
   news: News[] = [];
+
+  availableCompanies = [
+    { name: "Google", value: "Google" },
+    { name: "Microsoft", value: "Microsoft" },
+    { name: "Readdle", value: "Readdle" },
+    { name: "Apple", value: "Apple" },
+  ];
+
+  selectedCompany: { name: string, value: string } = this.availableCompanies[0];
+
   constructor(private newsService: NewsService) { }
 
   ngOnInit(): void {
-    this.newsService.getLatestNews("Google").subscribe((response:News[]) => {
-      this.news = response;
+    this.loadNews();
+  }
+
+  loadNews() {
+    this.newsService.getLatestNews(this.selectedCompany?.value).subscribe((news: News[]) => {
+      this.news = news;
       console.log(this.news);
     }, (error) => {
       console.log(error);
     });
   }
 
+  onCompanySelected(selectedCompany: { name: string, value: string }) {
+    this.selectedCompany = selectedCompany;
+    this.loadNews();
+  }
 }
