49ae5107832c97a00deb0413e3122ca36afb1192
Denys Leon <leonden0046@gmail.com>
2024-03-16 22:11:39 +0200

    Implemented Google auth UI

--- AssetsNet/src/app/components/google-auth/google-auth.component.ts
+++ AssetsNet/src/app/components/google-auth/google-auth.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CredentialResponse } from 'google-one-tap';
+import { AccountService } from 'src/app/_services/account.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-google-auth',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoogleAuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private accountService: AccountService,
+    private router: Router) { }
 
   ngOnInit(): void {
+      // @ts-ignore
+      google.accounts.id.initialize({
+        client_id: environment.clientId,
+        callback: this.handleCredentialResponse.bind(this),
+        auto_select: false,
+        cancel_on_tap_outside: true
+      });
+      // @ts-ignore
+      google.accounts.id.renderButton(
+        // @ts-ignore
+        document.getElementById("buttonDiv"),
+        // @ts-ignore
+        { theme: "filled_blue", size: "medium", width: "100%" }
+      );
+      // @ts-ignore
+      google.accounts.id.prompt((notification: PromptMomentNotification) => { });
+  }
+
+  async handleCredentialResponse(response: CredentialResponse) {
+    await this.accountService.loginWithGoogle(response.credential).subscribe(
+      (x: any) => {
+        this.accountService.setCurrentUser(x);
+        console.log(x);
+        window.location.href = '/home';
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
   }
 
 }
