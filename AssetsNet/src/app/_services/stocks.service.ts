71f10369d1387d88d0790cdc1a49110cc2abb074
Andrii <jaworovcki@gmail.com>
2024-03-19 17:19:04 +0200

    get the us stocks from the api

---
+++ AssetsNet/src/app/_services/stocks.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ExchangeSymbol } from '../models/stocks/exchangeSymbol';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StocksService {
+
+  eodhdApiUrl = environment.eodhdApiUrl;
+  eodhdApiToken = environment.eohdApiToken;
+
+  constructor(private http: HttpClient) { }
+
+  getExchangeSymbols() : Observable<ExchangeSymbol[]> {
+    let queryParams = new HttpParams();
+    queryParams = queryParams.append('api_token', this.eodhdApiToken);
+    queryParams = queryParams.append('fmt', 'json');
+    return this.http.get<ExchangeSymbol[]>(`${this.eodhdApiUrl}exchange-symbol-list/US`, { params: queryParams });
+  }
+
+  saveStockNames(symbols: ExchangeSymbol[]) {
+    let stockNames = symbols.map((symbol) => symbol.Name);
+    localStorage.setItem('stockNames', JSON.stringify(stockNames));
+  }
+
+}
