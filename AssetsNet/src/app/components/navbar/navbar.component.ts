5c0cce1bf3216a4065d76a8ad9bfccfb63bb1256
Denys Leon <leonden0046@gmail.com>
2024-03-19 09:11:24 +0200

    Added logout button

--- AssetsNet/src/app/components/navbar/navbar.component.ts
+++ AssetsNet/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from 'src/app/_services/account.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
   }
