20cc23e9a326ca53bad1c4d350392ad5c09a4f55
Andrii <jaworovcki@gmail.com>
2024-03-19 16:48:19 +0200

    create search component

--- AssetsNet/src/app/app.module.ts
+++ AssetsNet/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { RegisterComponent } from './components/account/register/register.compon
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
-import { GoogleAuthComponent } from './components/google-auth/google-auth.component';
 import { LatestNewsComponent } from './components/latest-news/latest-news.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { GoogleAuthComponent } from './components/account/google-auth/google-auth.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     HomeComponent,
     GoogleAuthComponent,
     LatestNewsComponent,
-    NavbarComponent
+    NavbarComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
