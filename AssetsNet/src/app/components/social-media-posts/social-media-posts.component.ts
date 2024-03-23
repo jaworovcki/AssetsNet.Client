4bf728cfa0f50bb5111b9e46f120a20d671525dd
Eldar Kerimov <goreldar@gmail.com>
2024-03-23 13:09:34 +0200

    added social media posts component

--- AssetsNet/src/app/components/social-media-posts/social-media-posts.component.ts
+++ AssetsNet/src/app/components/social-media-posts/social-media-posts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post/post';
+import { PostsService } from 'src/app/_services/posts.service';
 
 @Component({
   selector: 'app-social-media-posts',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SocialMediaPostsComponent implements OnInit {
 
-  constructor() { }
+  posts: Post[] = []
+  pageSize: number = 4;
+  currentPage: number = 1;
+
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
+		this.getMockPosts();
+  }
+
+  getMockPosts() {
+		this.posts = this.postsService.generateMockPostsArray(10);
+  }
+
+	getCurrentPosts(): Post[] {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    return this.posts.slice(startIndex, endIndex);
+  }
+
+  previousPage() {
+		if (this.currentPage > 1) {
+			this.currentPage--;
+		}
+  }
+
+  nextPage() {
+		const totalPages = Math.ceil(this.posts.length / this.pageSize);
+		if (this.currentPage < totalPages) {
+			this.currentPage++;
+		}
+  }
+
+  hasNextPage(): boolean {
+		const totalPages = Math.ceil(this.posts.length / this.pageSize);
+		return this.currentPage < totalPages;
+  }
+
+  hasPreviousPage(): boolean {
+		return this.currentPage > 1;
   }
 
 }
