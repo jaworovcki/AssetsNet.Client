1a760c89a8ba0f42df4ed5d4e6c689509f022fda
Denys Leon <leonden0046@gmail.com>
2024-03-29 10:57:46 +0200

    Persisting login and register

--- AssetsNet/src/app/app.component.ts
+++ AssetsNet/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserJwt } from './models/user/userJwt';
+import { AccountService } from './_services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AssetsNet';
+  userJwt: UserJwt | null = null;
+
+  constructor(private accountService: AccountService) {
+    this.accountService.currentUser$.subscribe((user) => {
+      this.userJwt = user;
+    })
+  }
+
+  ngOnInit(): void {
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user: UserJwt = JSON.parse(localStorage.getItem('user')!);
+    if (user) {
+      this.accountService.setCurrentUser(user);
+    }
+  }
 }
