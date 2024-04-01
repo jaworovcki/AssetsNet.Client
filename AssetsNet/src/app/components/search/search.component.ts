2e6707ca0678f341ee5a47387d8ff22cdc701eb6
Andrii <jaworovcki@gmail.com>
2024-04-02 00:58:40 +0300

    update the method to get the forecast

--- AssetsNet/src/app/components/search/search.component.ts
+++ AssetsNet/src/app/components/search/search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { Observable, debounceTime, map, startWith } from 'rxjs';
+import { Observable, catchError, debounceTime, forkJoin, map, of, startWith } from 'rxjs';
 import { ChatGptService } from 'src/app/_services/chat-gpt.service';
 import { NewsService } from 'src/app/_services/news.service';
 import { StocksService } from 'src/app/_services/stocks.service';
@@ -57,31 +57,60 @@ export class SearchComponent implements OnInit {
     if (this.stockFilter.value !== null && this.stockFilter.value !== '') {
       const stockName = this.stockFilter.value!.split(' ')[0];
       this.targetStock = stockName;
-      this.newsService.getRedditPosts(stockName).subscribe({
-        next: (redditPosts) => {
-          const posts = redditPosts.map((n) => n.title);
-          const gptRequest = this.chatGptService.constructGptRequest(posts);
-          this.chatGptQuery.query = gptRequest;
-          this.chatGptService.getChatGptResponse(this.chatGptQuery).subscribe({
-            next: (response) => {
-              console.log(response);
-              this.isResponseObtained = true;
-              this.simulateTyping(response.response);
-              this.stockFilter.setValue('');
-            },
-            error: (error) => {
-              console.log(error);
-            }
-          });
+  
+      // Use RxJS forkJoin to handle both API calls simultaneously and wait for both to finish
+      // If one fails, the other can still proceed
+      const redditPosts$ = this.newsService.getRedditPosts(stockName).pipe(
+        map(posts => posts.map(n => n.title)), // Map only the title for Reddit posts
+        catchError(error => {
+          console.error('Reddit fetch error:', error);
+          return of([]); // In case of error, return an empty array to keep the flow going
+        })
+      );
+  
+      const twitterPosts$ = this.newsService.getTwitterPosts(stockName).pipe(
+        map(posts => posts.map(n => n.text)), // Map only the text for Twitter posts
+        catchError(error => {
+          console.error('Twitter fetch error:', error);
+          return of([]); // In case of error, return an empty array to keep the flow going
+        })
+      );
+  
+      forkJoin([redditPosts$, twitterPosts$]).subscribe({
+        next: ([redditPosts, twitterPosts]) => {
+          // Combine posts from Reddit and Twitter
+          const combinedPosts = [...redditPosts, ...twitterPosts];
+          console.log('Combined posts:', combinedPosts);
+          // Check if combinedPosts is not empty
+          if (combinedPosts.length > 0) {
+            const gptRequest = this.chatGptService.constructGptRequest(combinedPosts);
+            this.chatGptQuery.query = gptRequest;
+            this.chatGptService.getChatGptResponse(this.chatGptQuery).subscribe({
+              next: (response) => {
+                console.log(response);
+                this.isResponseObtained = true;
+                this.simulateTyping(response.response);
+                this.stockFilter.setValue('');
+              },
+              error: (error) => {
+                console.error('ChatGPT fetch error:', error);
+              }
+            });
+          } else {
+            console.log('No relevant posts found on Reddit or Twitter.');
+            this.isResponseObtained = true;
+            this.aiResponse = 'Sorry, no relevant information about this stock on social media.';
+          }
         },
         error: (error) => {
-          console.log(error);
+          console.error('An unexpected error occurred:', error);
           this.isResponseObtained = true;
-          this.aiResponse = 'Sorry, no relevent information about this stock in the social media.';
+          this.aiResponse = 'An unexpected error occurred while fetching posts.';
         }
       });
     }
   }
+  
 
   simulateTyping(text: string, index: number = 0) {
     if (index < text.length) {
