7851a4d662084d3fea7f962b3cca0a99bc682038
Andrii <jaworovcki@gmail.com>
2024-04-28 16:17:26 +0300

    modify message model

--- AssetsNet/src/app/models/message.ts
+++ AssetsNet/src/app/models/message.ts
@@ -1,12 +1,12 @@
 export interface Message {
-    id: string; // for message id
-    content: string; // context of the message
-    dateRead: Date | null;
-    dateSent: Date;
-    senderId: string;
-    senderUsername: string;
-    senderPhotoUrl: string;
-    recipientId: string;
-    recipientUsername: string;
-    recipientPhotoUrl: string;
+  id: string; // for message id
+  content: string; // context of the message
+  dateRead: Date | null;
+  dateSent: Date;
+  senderId: string;
+  senderName: string;
+  senderPhotoUrl: string;
+  recipientId: string;
+  recipientName: string;
+  recipientPhotoUrl: string;
 }
