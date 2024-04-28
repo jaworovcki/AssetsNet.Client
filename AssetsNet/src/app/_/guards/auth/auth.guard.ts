8c860ef9b86ed961e3dd1d3b7f7872fbdfaa49d8
Denys Leon <leonden0046@gmail.com>
2024-04-28 13:08:03 +0300

    Implemented auth guard

--- AssetsNet/src/app/_/guards/auth/auth.guard.ts
+++ AssetsNet/src/app/_/guards/auth/auth.guard.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable, map } from 'rxjs';
+import { AccountService } from 'src/app/_services/account.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private accountService: AccountService, private toastr: ToastrService,
+    private router: Router) {
+
   }
-  
+  canActivate(): Observable<boolean> {
+    return this.accountService.currentUser$.pipe(
+      map(user => {
+        if (user) {
+          return true;
+        }
+        this.toastr.info("Unauthorized access");
+        this.router.navigateByUrl('/login');
+        return false;
+      })
+    )
+  }
 }
