45a5dc6c12091638f2446334536aead700e7cc2b
Denys Leon <leonden0046@gmail.com>
2024-04-30 20:10:56 +0300

    Created users-search component

---
+++ AssetsNet/src/app/_modals/user/users-search/users-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-search',
+  templateUrl: './users-search.component.html',
+  styleUrls: ['./users-search.component.scss']
+})
+export class UsersSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
