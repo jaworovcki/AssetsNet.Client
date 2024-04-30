3aee4e9cc9b9ba34c47b93f3cd1df26a22487b1d
Denys Leon <leonden0046@gmail.com>
2024-04-30 20:22:54 +0300

    Basic interactive input on search component

--- AssetsNet/src/app/_modals/user/users-search/users-search.component.ts
+++ AssetsNet/src/app/_modals/user/users-search/users-search.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersSearchComponent implements OnInit {
 
+  searchUserNameTerm: string = '';
   constructor() { }
 
   ngOnInit(): void {
