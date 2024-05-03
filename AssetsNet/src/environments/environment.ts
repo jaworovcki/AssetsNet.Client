6aa8cb8921495cba75c80e00b89c4cd13b37298d
Andrii <jaworovcki@gmail.com>
2024-05-03 21:25:32 +0300

    add tariffPlans to the environment

--- AssetsNet/src/environments/environment.ts
+++ AssetsNet/src/environments/environment.ts
@@ -2,6 +2,8 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { TariffPlan } from "src/app/models/tariffPlan/tariffPlan";
+
 export const environment = {
   production: false,
   baseUrl: 'https://localhost:7002/api/',
@@ -9,6 +11,11 @@ export const environment = {
   eodhdApiUrl:"https://eodhd.com/api/",
   eohdApiToken:"660af0f7449555.28199836",
   clientId: '491184798714-ahvlhl23coq1l5sgk7cg75ta48aqdclr.apps.googleusercontent.com',
+  tariffPlans: [
+    new TariffPlan(0, "Стандарт", 0, "Отримайте безкоштовний доступ до нашого застосунку з 100Мб для збереження ваших улюблених фото та відео."),
+    new TariffPlan(1, "Преміум", 100, "Максимізуйте свої можливості! З планом Преміум отримайте 200Мб лише за 100 гривень. Більше місця - більше вражень!"),
+    new TariffPlan(2, "Голд", 300, "Оберіть Голд і отримайте абсолютний простір - 500Мб для зберігання вашого контенту. Зберігайте усі свої цінні спогади!"),
+  ] as TariffPlan[],
 };
 
 /*
