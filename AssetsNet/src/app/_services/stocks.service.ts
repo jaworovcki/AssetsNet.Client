930dd94c1090d69f3ebc5f2918f7a5a1c621a5d4
Andrii <jaworovcki@gmail.com>
2024-05-16 19:26:00 +0300

    updateh output path

--- AssetsNet/src/app/_services/stocks.service.ts
+++ AssetsNet/src/app/_services/stocks.service.ts
@@ -10,28 +10,12 @@ import { Stock } from '../models/stocks/stock';
 })
 export class StocksService {
 
-  eodhdApiUrl = environment.eodhdApiUrl;
-  eodhdApiToken = environment.eohdApiToken;
-
   baseUrl: string = environment.baseUrl;
 
   constructor(private http: HttpClient) { }
 
-  getExchangeSymbols(): Observable<ExchangeSymbol[]> {
-    let queryParams = new HttpParams();
-    queryParams = queryParams.append('api_token', this.eodhdApiToken);
-    queryParams = queryParams.append('fmt', 'json');
-    return this.http.get<ExchangeSymbol[]>(`${this.eodhdApiUrl}exchange-symbol-list/US`, { params: queryParams });
-  }
-
-  saveStockNames(symbols: ExchangeSymbol[]): string[] | null {
-    let stockNames = symbols.map((symbol) => symbol.Name);
-    localStorage.setItem('stockNames', JSON.stringify(stockNames));
-    return stockNames;
-  }
-
-  checkIfStockNamesExistsInLocalStorage(): boolean {
-    return localStorage.getItem('stockNames') !== null;
+  getExchangeSymbols(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.baseUrl}stocks/stock-names-list`);
   }
 
   getStocks(symbols: string[]) {
